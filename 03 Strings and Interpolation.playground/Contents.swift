70fe2b50a182bef14855292828f76b2e93ab43ef
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2022-10-20 01:50:37 +0600

    Add file

---
+++ 03 Strings and Interpolation.playground/Contents.swift
@@ -0,0 +1,9 @@
+var greeting = "Hello, Playground"
+var userName: String = "ShuvroWritesCode!"
+var age = 21
+
+var myFinalNotice = "My ID is \(age) and I am from Bangladesh"
+var result = greeting + " " + userName
+
+print(myFinalNotice + "\n" + result)
+
