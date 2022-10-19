b27c0ce5fa0e7f86d83218aa6b2f50755a65a23d
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2022-10-20 01:40:31 +0600

    Add file

---
+++ 02 Operators and Range.playground/Contents.swift
@@ -0,0 +1,18 @@
+//command+slash to comment a text
+
+/*
+ Operators: +, -, *, /, %
+ Assignment Operators: +=, -=, *=, /=
+ Comperative Operators: >>, <<, ==, <=, >=, !=
+*/
+
+var result = 4 + 4
+print(result == 9)
+
+// Range: ...
+
+var myRange = 1...5
+print(myRange.contains(4))
+
+var myRange = 1..<5
+print(myRange.contains(5))
