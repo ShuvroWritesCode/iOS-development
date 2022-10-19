7764d94ac0975dd66d4b0e23a604e212f39ee1ed
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2022-10-20 02:34:40 +0600

    Add file

---
+++ 05 Type Conversion.playground/Contents.swift
@@ -0,0 +1,15 @@
+var googleStock = 12.5
+
+//let userBuyStock = googleStock + 1
+//let userBuyStock = 1 + googleStock
+let userBuyStock:Int = Int(googleStock)
+
+let userAge = "55"
+
+let userAgeinSystem = Int(userAge)
+
+let storedValue = (userAgeinSystem ?? 0) + 5
+
+
+
+
