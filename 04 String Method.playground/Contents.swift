76464b88c06b38b6bbc9ab8d9983b125b42c75e3
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2022-10-20 02:00:35 +0600

    Add file

---
+++ 04 String Method.playground/Contents.swift
@@ -0,0 +1,9 @@
+var greeting = "Hello, World!"
+
+//Most used methods in string
+greeting.count
+greeting.contains("o")
+greeting.isEmpty
+greeting.removeLast()
+greeting.split(separator: ",")
+greeting.removeAll()
