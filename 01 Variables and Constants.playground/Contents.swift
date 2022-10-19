9f83b625c1da36d41ef0f8c7f40595f14181c67b
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2022-10-20 01:07:51 +0600

    Add file

---
+++ 01 Variables and Constants.playground/Contents.swift
@@ -0,0 +1,19 @@
+var userName = "ShuvroWritesCode"
+let accountId = 2000123
+
+userName = "Shuvro Writes Code"
+
+var numberOfLoginsInADay: Int = 34
+
+var name, email, address: String
+var BTCValue, EthValue: Double
+
+name = "Shuvro"
+email = "abc012@xyz.com"
+address = "Dhaka, Bangladesh"
+
+BTCValue = 1239.234
+EthValue = 9991233.012345678
+
+print(userName, accountId, numberOfLoginsInADay, name, email, address, BTCValue, EthValue)
+
