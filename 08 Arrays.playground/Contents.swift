aa28659f9fd2293ed378fbc865ba80a23e8404d6
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2022-10-21 01:23:41 +0600

    Add file

---
+++ 08 Arrays.playground/Contents.swift
@@ -0,0 +1,17 @@
+var marvelHeroes: [String] = ["Ironman", "Thor", "Captain America"]
+//var DCHeroes: Array<String> = Array<String>()
+
+marvelHeroes.count
+marvelHeroes.isEmpty
+
+marvelHeroes[0]
+marvelHeroes[0...2]
+
+marvelHeroes.append("Hulk")
+marvelHeroes.insert("Spiderman", at: 3)
+
+marvelHeroes.sort()
+marvelHeroes.reverse()
+marvelHeroes.sorted()
+marvelHeroes.reversed()
+
