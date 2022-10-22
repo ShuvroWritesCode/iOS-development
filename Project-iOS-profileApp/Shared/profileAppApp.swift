73b8592d41b96b90525cf497cf29fcd70f6ec86a
Iftekharul Haque Shuvro <91349513+ShuvroWritesCode@users.noreply.github.com>
2022-10-22 23:54:01 +0600

    Add Practice-Project1

---
+++ Project-iOS-profileApp/Shared/profileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileAppApp.swift
+//  Shared
+//
+//  Created by Haque Iftekharul on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct profileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
